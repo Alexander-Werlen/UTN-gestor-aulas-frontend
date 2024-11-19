363d8cfc862c9479dc94ebbebffc1807091aa829
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2024-11-19 19:26:54 -0300

    fix: add bedel_id to request

--- src/services/reservaService.jsx
+++ src/services/reservaService.jsx
@@ -5,15 +5,18 @@ const baseUrlEsporadica = "reservas/esporadica"
 const baseUrlPeriodica = "reservas/periodica"
 
 export const postReservaEsporadica = (data) => {
+    data.bedel_id = localStorage.getItem("id")
     return api.post(baseUrlEsporadica, data)
 }
 
 export const postReservaPeriodica = (data) => {
+    data.bedel_id = localStorage.getItem("id")
     return api.post(baseUrlPeriodica, data)
 }
 
 
 export const postReserva = (data, tipo_reserva) => {
+    data.bedel_id = localStorage.getItem("id")
     let baseUrl = tipo_reserva.toLowerCase() === "esporadica" ? baseUrlEsporadica : baseUrlPeriodica
     return api.post(baseUrl, data)
 }
